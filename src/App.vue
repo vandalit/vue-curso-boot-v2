ee3823692c961743665e2bb7e7c9a3a5fc7b20a6
Van <84937597+vandalit@users.noreply.github.com>
2024-12-30 22:31:56 -0300

    bootstrap despliegue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-
+    <div class="container mt-5">
+        <h1 class="text-center text-primary">
+            ¡Hola desde Vue + Bootstrap!
+        </h1>
+    </div>
 </template>
-
-<script></script>
-
-<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hola desde hola cosa</h1>
+</template>
