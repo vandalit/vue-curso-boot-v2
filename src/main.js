9414bc71114fe0e3f13e30139bff5a285c24eacd
Van <84937597+vandalit@users.noreply.github.com>
2024-12-30 22:26:47 -0300

    bootstrap improtado

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Importar Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importar opcionalmente Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Importar Bootstrap Icons (opcional)
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 createApp(App).use(store).use(router).mount('#app')
