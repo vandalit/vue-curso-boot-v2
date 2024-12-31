b6e87ab3258ca387196a4168bcd755ded43b0b7e
Van <84937597+vandalit@users.noreply.github.com>
2024-12-30 22:17:07 -0300

    limpieza

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-    <h1>hola desde app</h1>
-    <router-view></router-view>
+
 </template>
 
 <script></script>
--- src/components/HolaCosa.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>hola desde componente</h1>
-</template>
-
-<script>
-export default {
-    name: 'HolaCosa',
-}
-</script>
